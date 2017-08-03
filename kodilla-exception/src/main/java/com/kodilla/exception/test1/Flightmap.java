d17be93b7c02762f2ab11029c3e2d792dc369ab5
marcinmuller <mmuller@o2.pl>
2017-08-03 19:00:14 +0200

    Test6

--- kodilla-exception/src/main/java/com/kodilla/exception/test1/Flightmap.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test1/Flightmap.java
@@ -7,16 +7,19 @@ import java.util.Map;
  * Created by Marcin Muller on 02.08.17.
  */
 public class Flightmap {
-    public void findFilght(Flight flight) throws RouteNotFoundException{
-        Map<String,Boolean> map=new HashMap<>();
-        map.put("Katania",true);
-        map.put("Palermo",true);
-        map.put("Syrakuzy",false);
-        map.put("Trapani",true);
-        if(map.containsKey(flight.getDestination())){
-            if( map.get(flight.getDestination()))System.out.println(flight.getOrigin()+" ---> "+flight.getDestination());
-            else System.out.println(flight.getOrigin()+" -/-> "+flight.getDestination());
-        }else throw new RouteNotFoundException(flight.getDestination());
+    public void findFilght(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("Katania", true);
+        map.put("Palermo", true);
+        map.put("Syrakuzy", false);
+        map.put("Trapani", true);
+        if (map.containsKey(flight.getDestination())) {
+            if (map.get(flight.getDestination()))
+                System.out.println(flight.getOrigin() + " ---> " + flight.getDestination());
+            else System.out.println(flight.getOrigin() + " -/-> " + flight.getDestination());
+        } else {
+            throw new RouteNotFoundException(flight.getDestination());
+        }
 
     }
 }
