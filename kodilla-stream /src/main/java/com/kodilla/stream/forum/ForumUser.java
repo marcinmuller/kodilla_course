2d46f168146e9fe5cece3a3a805bf1339a56d7d4
marcinmuller <mmuller@o2.pl>
2017-08-03 18:03:25 +0200

    mod. 8

--- kodilla-stream /src/main/java/com/kodilla/stream/forum/ForumUser.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.stream.forum;
-
-/**
- * Created by Marcin Muller on 26.07.17.
- */
-public class ForumUser {
-}
--- kodilla-stream /src/test/java/com/kodilla/stream/person/PeopleTest.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.stream.person;
-
-/**
- * Created by Marcin Muller on 27.07.17.
- */
-public class PeopleTest {
-}
