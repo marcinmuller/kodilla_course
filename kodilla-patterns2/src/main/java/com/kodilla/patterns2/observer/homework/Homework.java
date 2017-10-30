f00b56b762f25542c17ac52d27231ee7667949ad
marcinmuller <mmuller@o2.pl>
2017-10-30 20:04:02 +0100

    mod 20

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Homework.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Homework.java
@@ -4,10 +4,12 @@ package com.kodilla.patterns2.observer.homework;
  * Created by Marcin Muller on 30.10.17.
  */
 public class Homework {
+    private String student;
     private String contents;
     private int grade;
 
-    public Homework(String contents) {
+    public Homework(String student, String contents) {
+        this.student = student;
         this.contents = contents;
     }
 
@@ -23,4 +25,21 @@ public class Homework {
         this.grade = grade;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Homework homework = (Homework) o;
+
+        if (student != null ? !student.equals(homework.student) : homework.student != null) return false;
+        return contents != null ? contents.equals(homework.contents) : homework.contents == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = student != null ? student.hashCode() : 0;
+        result = 31 * result + (contents != null ? contents.hashCode() : 0);
+        return result;
+    }
 }
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/HomeworkTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/HomeworkTestSuite.java
@@ -10,22 +10,42 @@ public class HomeworkTestSuite {
     @Test
     public void testHomework(){
         //given
-        Homework homework1 = new Homework("111");
-        Homework homework2 = new Homework("222");
-        Homework homework3 = new Homework("333");
-        Homework homework4 = new Homework("444");
-        HomeworkQueue homeworkQueue = new HomeworkQueue("k1");
-        Mentor mentor1 = new Mentor("nn");
-        homeworkQueue.registerObserver(mentor1);
+        Homework homework1 = new Homework("111","k1");
+        Homework homework2 = new Homework("222","k1");
+        Homework homework3 = new Homework("333","k1");
+        Homework homework4 = new Homework("444","k1");
+        Homework homework5 = new Homework("777","k2");
+        Homework homework6 = new Homework("444","k2");
+        Homework homework7 = new Homework("777","k3");
+        Homework homework8 = new Homework("333","k3");
+        Homework homework9 = new Homework("444","k3");
+        HomeworkQueue homeworkQueue1 = new HomeworkQueue("k1");
+        HomeworkQueue homeworkQueue2 = new HomeworkQueue("k2");
+        HomeworkQueue homeworkQueue3 = new HomeworkQueue("k3");
+        Mentor mentor1 = new Mentor("nn1");
+        Mentor mentor2 = new Mentor("nn2");
+        homeworkQueue1.registerObserver(mentor1);
+        homeworkQueue2.registerObserver(mentor2);
+        homeworkQueue3.registerObserver(mentor2);
+
         //when
-        homeworkQueue.addHomework(homework1);
-        homeworkQueue.addHomework(homework2);
-        homeworkQueue.addHomework(homework3);
-        homeworkQueue.addHomework(homework4);
+        homeworkQueue1.addHomework(homework1);
+        homeworkQueue1.addHomework(homework2);
+        homeworkQueue1.addHomework(homework3);
+        homeworkQueue1.addHomework(homework4);
+
+        homeworkQueue2.addHomework(homework5);
+        homeworkQueue2.addHomework(homework6);
+
+        homeworkQueue3.addHomework(homework7);
+        homeworkQueue3.addHomework(homework8);
+        homeworkQueue3.addHomework(homework9);
+
         mentor1.giveAGrade(homework2,2);
         mentor1.giveAGrade(homework3,5);
 
         //then
         Assert.assertEquals(2,mentor1.getHomeworkSet().size());
+        Assert.assertEquals(5,mentor2.getHomeworkSet().size());
     }
 }
