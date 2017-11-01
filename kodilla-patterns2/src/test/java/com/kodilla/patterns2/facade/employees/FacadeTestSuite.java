4365d60194d3b316c64dc63a66e8c7faec37782a
marcinmuller <mmuller@o2.pl>
2017-11-01 08:15:23 +0100

    mod 20.1

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/employees/FacadeTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/employees/FacadeTestSuite.java
@@ -25,11 +25,15 @@ public class FacadeTestSuite {
 
     Company xxx = new Company("xxx");
     Company yyy = new Company("yyy");
+    Company zzzyyy = new Company("zzzyyy");
 
     Employee ab = new Employee("aa", "bbb");
     Employee cd = new Employee("cc", "ddd");
     Employee ef = new Employee("ee", "fff");
     Employee gg = new Employee("gg", "ggg");
+    Employee ggaa = new Employee("aa", "ggar");
+    Employee ggz = new Employee("gg", "zzz");
+
 
     @Before
     public void given() {
@@ -37,11 +41,17 @@ public class FacadeTestSuite {
         xxx.getEmployees().add(cd);
         yyy.getEmployees().add(ef);
         xxx.getEmployees().add(gg);
+        xxx.getEmployees().add(ggaa);
+        yyy.getEmployees().add(ggz);
+        zzzyyy.getEmployees().add(ab);
 
         ab.getCompanies().add(xxx);
+        ab.getCompanies().add(zzzyyy);
         cd.getCompanies().add(xxx);
         ef.getCompanies().add(yyy);
         gg.getCompanies().add(xxx);
+        ggaa.getCompanies().add(xxx);
+        ggz.getCompanies().add(yyy);
 
         companyDao.save(xxx);
         companyDao.save(yyy);
@@ -50,6 +60,10 @@ public class FacadeTestSuite {
         employeeDao.save(cd);
         employeeDao.save(ef);
         employeeDao.save(gg);
+        employeeDao.save(ggz);
+        employeeDao.save(ggaa);
+
+
     }
 
     @Test
@@ -58,7 +72,7 @@ public class FacadeTestSuite {
     //when
         int result=searchFacade.processSearchEmployee("gg").size();
     //then
-        Assert.assertEquals(1,result);
+        Assert.assertEquals(3,result);
     }
 
     @Test
@@ -70,14 +84,26 @@ public class FacadeTestSuite {
         Assert.assertEquals(0,result);
     }
 
-    @After
+    @Test
+    public void testCompany2(){
+        //given
+        //when
+        int result=searchFacade.processSearchCompany("yy").size();
+        //then
+        Assert.assertEquals(2,result);
+    }
+
+   @After
     public void cleanUp(){
         companyDao.delete(xxx);
         companyDao.delete(yyy);
+        companyDao.delete(zzzyyy);
         employeeDao.delete(ab);
         employeeDao.delete(cd);
         employeeDao.delete(ef);
         employeeDao.delete(gg);
+        employeeDao.delete(ggaa);
+        employeeDao.delete(ggz);
     }
 
 }
